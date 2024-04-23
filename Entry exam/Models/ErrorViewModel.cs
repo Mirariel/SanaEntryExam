4592641cbda852923e58fe96dd3c4517b60cc32f
Дмитро Окоча <ipz234_odp@student.ztu.edu.ua>
2024-04-23 23:45:36 +0300

    Вступний екзамен

---
+++ Entry exam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Entry_exam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
